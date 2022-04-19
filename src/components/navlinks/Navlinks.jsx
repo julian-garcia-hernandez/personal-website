ba684701d049397faed8d3634c4bd20f96d52b59
Julian Garcia-Hernandez <julian.garcia.hernandez11@gmail.com>
2022-04-19 15:12:14 -0500

    created navlinks component

---
+++ src/components/navlinks/Navlinks.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './navlinks.css'
+const Navlinks = () => {
+  return (
+    <nav className='info-links'>
+            <ul>
+              <li><a href="#">About</a></li>
+              <li><a href="#">Socials</a></li>
+              <li><a href="#">Contact</a></li>
+            </ul>
+    </nav>
+  )
+}
+
+export default Navlinks
