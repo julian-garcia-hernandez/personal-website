8697dd92df83aa0364e187b6189c36b8495e3732
Julian Garcia-Hernandez <julian.garcia.hernandez11@gmail.com>
2022-04-19 15:06:44 -0500

    about component for my bio

---
+++ src/components/about/About.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './about.css';
+const About = () => {
+  return (
+    <div className='about'>
+      <p className='about-text'>
+        I go by he, him, his pronouns.<br></br>
+        I hail from Alton, Texas.<br></br>
+        I am 21 years old.<br></br>
+        I am currently a studying Computer Science at the University of North Texas.<br></br>
+        I have created projects with REACT, Python, C, C++.<br></br>
+        I love to go the gym, journal, and sing (only when I am alone though).<br></br>
+      </p>
+    </div>
+  )
+}
+
+export default About
