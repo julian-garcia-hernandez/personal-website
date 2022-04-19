225458bba5f406dacaa7d57d49c51c11abd376a5
Julian Garcia-Hernandez <julian.garcia.hernandez11@gmail.com>
2022-04-19 15:08:08 -0500

    changed one class name to a more suitable name

--- src/components/emailForm/EmailForm.jsx
+++ src/components/emailForm/EmailForm.jsx
@@ -7,7 +7,7 @@ const EmailForm = () => {
             <input className='input-username' type='text' name='user_name' placeholder='Name' />
             <input className='input-email' type='email' name='user_email' placeholder='Email' />
             <textarea className='textarea-message' name='message' placeholder='Message' />
-            <input type='submit' value='Send' />
+            <input className='btn-submit' type='submit' value='Send' />
         </form>
     </div>
     )
