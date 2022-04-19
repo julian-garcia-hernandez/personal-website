901836add967465cfc5532b8b2d1c0304f7d965f
Julian Garcia-Hernandez <julian.garcia.hernandez11@gmail.com>
2022-04-19 15:13:22 -0500

    created socials' component

---
+++ src/components/socials/Socials.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './socials.css'
+const Socials = () => {
+  return (
+    <nav className='socials-links'>
+      <ul>
+        <li><a href='#'>LinkedIn</a></li>
+        <li><a href='#'>Github</a></li>
+        <li><a href='#'>LinkedIn</a></li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Socials
