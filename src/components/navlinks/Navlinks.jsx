77ba018e18a4990016706c647c4de972e3a3727d
Julian Garcia-Hernandez <julian.garcia.hernandez11@gmail.com>
2022-04-20 18:20:53 -0500

    added a section  for my resume

--- src/components/navlinks/Navlinks.jsx
+++ src/components/navlinks/Navlinks.jsx
@@ -3,11 +3,12 @@ import './navlinks.css'
 const Navlinks = () => {
   return (
     <nav className='info-links'>
-            <ul>
-              <li><a href="#">About</a></li>
-              <li><a href="#">Socials</a></li>
-              <li><a href="#">Contact</a></li>
-            </ul>
+      <ul>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Resume</a></li>
+        <li><a href="#">Socials</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
     </nav>
   )
 }
