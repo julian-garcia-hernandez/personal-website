7ea5110233063b54cd287e828baae5286103b352
Julian Garcia-Hernandez <julian.garcia.hernandez11@gmail.com>
2022-04-21 12:31:45 -0500

    new resume component

--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -4,7 +4,22 @@ import React from 'react'
 
 const Resume = () => {
   return (
-    <div>Resume</div>
+    <div className='resume'>
+      <h3>Projects</h3>
+      <ul>
+        <li>Goobla Academy</li>
+        <li>Crowate</li>
+        <li>Personal Website</li>
+      </ul>
+      <h3>Skills</h3>
+      <ul>
+        <li>C++</li>
+        <li>C</li>
+        <li>Python</li>
+        <li>React</li>
+        <li>C++</li>
+      </ul>
+    </div>
   )
 }
 
